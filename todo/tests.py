becf0e341f8e9ae04375bc6b4f53e2ce82a12c61
1F10230084 <s1f102300841@iniad.org>
2024-07-04 11:30:10 +0900

    テストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
@@ -35,3 +35,21 @@ class TaskModelCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+
+class TodoViewTestCase(TestCase):
+    def test_index_get(self):
+        client = Client()
+        responce = client.get('/')
+
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(responce.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(responce.context['tasks']), 0)
+
+    def test_index_post(self):
+        client = Client()
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        responce = client.post('/', data)
+
+        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(responce.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(responce.context['tasks']), 1)
